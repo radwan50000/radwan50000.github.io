194daf1f811d81170e16f176da6c9f482e645f65
Muhammed <muhammedelsayedradwan5@gmail.com>
2023-12-03 17:24:36 +0200

    Add changes to javascript file of home page

--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,7 @@ let langButton = document.querySelector("#lang-select"),
     main = document.querySelector("main"),
     seatNumButton = document.querySelector("#seat-number"),
     resultsButton = document.querySelector("#results"),
+    attr = 'ar',
     english = false;
 
 
@@ -10,22 +11,37 @@ function changeLanguage(){
     if(!english){
         main.style.cssText = "animation: Ar_to_En_main 0.6s ease-in-out forwards !important;";
         selectedLangBox.style.cssText = "transform: translateX(-100%);";
-        selectedLangBox.textContent = "En";
-        seatNumButton.textContent = 'رقم الجلوس';
-        resultsButton.textContent = 'نتيجة الفصل الدراسي';
+        
     }else{
         main.style.cssText = "animation: En_to_Ar_main 0.6s ease-in-out forwards !important;";
         selectedLangBox.style.cssText = "transform: translateX(0%);";
-        seatNumButton.textContent = 'sitting number';
-        resultsButton.textContent = 'Semester result';
-        selectedLangBox.textContent = "ع";
     }
+    selectedLangBox.textContent = language[attr].langBox;
+    seatNumButton.textContent = language[attr].seatNumButton;
+    resultsButton.textContent = language[attr].resultsButton;
     
+}
+
+let language = {
+    en: {
+        langBox: 'ع',
+        resultsButton: 'Semester Result',
+        seatNumButton: 'Sitting Number',
+    },
+    ar: {
+        langBox: 'En',
+        resultsButton: 'نتيجة الفصل الدراسي',
+        seatNumButton: 'رقم الجلوس',
+    }
+
 }
 
 langButton.addEventListener("click",function(){
     english = english == true ? false:true;
+    attr = english ? 'en':'ar';
     changeLanguage();
 });
 
 changeLanguage();
+
+console.log(language);
