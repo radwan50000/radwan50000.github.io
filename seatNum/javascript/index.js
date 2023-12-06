e455cba7eef2f110d284dd1959d48abfba788189
Muhammed <muhammedelsayedradwan5@gmail.com>
2023-12-06 08:01:47 +0200

    seat-number page add javascript validation no.1

--- seatNum/javascript/index.js
+++ seatNum/javascript/index.js
@@ -123,6 +123,27 @@ input_field.addEventListener("keyup",function(){
     enableSubmitButton();
 })
 
+input_field.addEventListener('keydown',function(e){
+    if((input_field.value+'').length > 3 ){
+        if(!(e.code == 'Backspace' || e.code == 'Delete')){
+            e.preventDefault();
+        }
+    }
+})
+
+//Handle If User Enter Full Number By Paste it 
+
+let input = document.getElementById('input-field');
+
+input.addEventListener('keydown',function(event){
+    if((input.value+'').length > 3 ){
+        if(!(event.key == 'Backspace' || event.key == 'Delete')){
+            event.preventDefault();
+        }else if ((input.value+'').length > 4){
+            input.value = input.value.slice(0,4);
+        }
+    }
+})
 
 
 async function getStudentData() {
